6a92ac5ac10fa3f5c98c6d17a021751c73a7b628
telahdihapus <ferdinanprasetya@gmail.com>
2017-11-24 20:07:18 +0700

    versi 1

--- bot.py
+++ bot.py
@@ -6,30 +6,33 @@ log = log_.read()
 log_.close()
 updates = bot.get_updates().wait()
 pan = len(updates)
-i = 0
-mulai = 0
-for updates_ in updates:
+#i = 0
+#mulai = 0
+#for updates_ in updates:
 	#print(log, updates[i].update_id)
-	if str(updates[i].update_id) == str(log):
-		mulai = i+1
-		break
-	i += 1
+	#if str(updates[i].update_id) == str(log):
+	#	mulai = i+1
+	#	break
+	#i += 1
+trk_ = 0
 while True:
 	updates = bot.get_updates().wait()
 	pan = len(updates)
-	if pan > mulai:
+	trk = len(updates)
+	if trk_ > trk:
+		trk_ = trk
 		#print(pan, '>', mulai)
-		if not log == str(updates[pan-1].update_id):
-			save = open('log.txt', 'w+')
-			save.write(str(updates[mulai].update_id))
-			save.close()
-			user_id = updates[mulai].message.chat.id
+		#if not log == str(updates[pan-1].update_id):
+			#save = open('log.txt', 'w+')
+			#save.write(str(updates[mulai].update_id))
+			#save.close()
+		user_id = updates[trk-1].message.chat.id
 			#print(user_id)
-			if updates[mulai].message.text == '/start':
-				result = bot.send_message(user_id, 'terimakasih telah memulai').wait()
-			elif updates[mulai].message.text == '/info':
-				result = bot.send_message(user_id, 'ini adalah bot percobaan ^_^').wait()
-			else:
-				result = bot.send_message(user_id, 'command kamu salah kakak :P').wait()
-			mulai += 1
+		if updates[trk-1].message.text == '/start':
+			result = bot.send_message(user_id, 'terimakasih telah memulai').wait()
+		elif updates[trk-1].message.text == '/info':
+			result = bot.send_message(user_id, 'ini adalah bot percobaan ^_^').wait()
+		else:
+			result = bot.send_message(user_id, 'command kamu salah kakak :P').wait()
+			#mulai += 1
 #print([u.message.sender.id for u in updates])
