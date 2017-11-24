0e133070f6be32df5485c3efee69c497b167d258
telahdihapus <ferdinanprasetya@gmail.com>
2017-11-24 18:28:27 +0700

    versi pertama 2

--- bot.py
+++ bot.py
@@ -7,8 +7,9 @@ log_.close()
 updates = bot.get_updates().wait()
 pan = len(updates)
 i = 0
+mulai = 0
 for updates_ in updates:
-	print(log, updates[i].update_id)
+	#print(log, updates[i].update_id)
 	if str(updates[i].update_id) == str(log):
 		mulai = i+1
 		break
@@ -17,13 +18,13 @@ while True:
 	updates = bot.get_updates().wait()
 	pan = len(updates)
 	if pan > mulai:
-		print(pan, '>', mulai)
+		#print(pan, '>', mulai)
 		if not log == str(updates[pan-1].update_id):
 			save = open('log.txt', 'w+')
 			save.write(str(updates[mulai].update_id))
 			save.close()
 			user_id = updates[mulai].message.chat.id
-			print(user_id)
+			#print(user_id)
 			if updates[mulai].message.text == '/start':
 				result = bot.send_message(user_id, 'terimakasih telah memulai').wait()
 			elif updates[mulai].message.text == '/info':
